3541aaf2a119ddc7f3856594ae9685b8c03bef2a
Ilton Batista <43584705+IltonBJSilva@users.noreply.github.com>
2025-05-22 20:48:43 -0300

    Deleção de arquivos

--- services/extrato.py
+++
@@ -1,7 +0,0 @@
-from utils.formatador import formatar_valor
-
-def gerar_extrato(conta):
-    valor = conta["saldo"]
-    valor_formatado = formatar_valor(valor)
-    extrato = conta["extrato"]
-    return extrato, valor_formatado
