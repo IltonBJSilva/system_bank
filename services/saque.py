3037f2dc22e37ccee62c8503b1f290747fe95055
Ilton Batista <43584705+IltonBJSilva@users.noreply.github.com>
2025-05-22 20:48:18 -0300

    Função de saque do projeto

--- services/saque.py
+++ services/saque.py
@@ -1,10 +1,9 @@
-def realizar_saque(conta):
-    valor = float(input('Digite o valor a ser sacado: '))
+def realizar_saque(conta, valor):
     conta["saldo"] -= valor
     conta["saques_diarios"] += 1
-    
-    conta["extrato"].append(f"Saque: R$ {valor}")
-    conta["extrato"].append(f"Saque Diario: R$ {conta["saques_diarios"]}")
 
-    print(f'Valor R$:{valor} sacado com sucesso')
+    conta["extrato"].append(f"Saque: R$ {valor:.2f}")
+    conta["extrato"].append(f"Saque Diário nº: {conta['saques_diarios']}")
+
+    print(f'Valor R$:{valor:.2f} sacado com sucesso')
     return conta
